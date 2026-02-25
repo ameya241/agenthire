1ea7608089a58ff94c970156459b75a2e8871179
ameya241 <142866981+ameya241@users.noreply.github.com>
2026-02-25 15:26:59 +0530

    Removed pipeline and loaded model manually

--- agent.py
+++ agent.py
@@ -1,9 +1,10 @@
-from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import torch
 
-generator = pipeline(
-    task="text2text-generation",
-    model="google/flan-t5-small"
-)
+model_name = "google/flan-t5-small"
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
 def analyze_with_agent(resume, jd):
 
@@ -23,9 +24,11 @@ Job Description:
 {jd}
 """
 
-    result = generator(
-        prompt,
+    inputs = tokenizer(prompt, return_tensors="pt", truncation=True)
+
+    outputs = model.generate(
+        **inputs,
         max_new_tokens=200
     )
 
-    return result[0]["generated_text"]
+    return tokenizer.decode(outputs[0], skip_special_tokens=True)
